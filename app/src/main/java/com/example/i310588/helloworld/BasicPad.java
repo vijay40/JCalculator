9067adf776dd41842141ab930c50c4ea269275ed
vijay40 <jainvijay40@gmail.com>
2015-03-21 15:57:47 +0530

    basic pad fragment java

---
+++ app/src/main/java/com/example/i310588/helloworld/BasicPad.java
@@ -0,0 +1,20 @@
+package com.example.i310588.helloworld;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by I310588 on 3/16/2015.
+ * Contains functionality of Basic pad fragment
+ */
+public class BasicPad extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.basic_pad, container, false);
+    }
+}
